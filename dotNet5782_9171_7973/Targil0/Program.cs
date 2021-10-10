7abe2e0149ebe09bd6b0ff59f646aac7173589fe
dasizis <dasizisman480@gmail.com>
2021-10-10 14:37:15 +0300

    initial code - welcome function

---
+++ dotNet5782_9171_7973/Targil0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Targil0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9171();
+        }
+
+        private static void Welcome9171()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"{name}, welcome to my first console application");
+        }
+        private static void Welcome7973()
+        {
+
+        }
+    }
+}

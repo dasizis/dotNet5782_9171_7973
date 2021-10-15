ad56d8ae237c3514d1ec8267ea5d7b9763652ceb
yaeldoch <92118469+yaeldoch@users.noreply.github.com>
2021-10-15 10:55:50 +0300

    added all structs.

---
+++ dotNet5782_9171_7973/ConsuleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using IDAL.DO;
+
+namespace ConsuleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           BaseStation baseStation = new BaseStation();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

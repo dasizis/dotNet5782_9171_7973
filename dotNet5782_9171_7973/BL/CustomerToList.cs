387ec3662cc848382070add5d558b1df8075bae7
yaeldoch <92118469+yaeldoch@users.noreply.github.com>
2021-11-03 16:12:24 +0200

    added CustomerToList class

---
+++ dotNet5782_9171_7973/BL/CustomerToList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class CustomerToList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public int ParcelsSendAndSupplied { get; set; }
+        public int ParcelsSendAndNotSupplied { get; set; }
+        public int ParcelsRecieved { get; set; }
+        public int ParcelsOnWay { get; set; }
+
+    }
+}

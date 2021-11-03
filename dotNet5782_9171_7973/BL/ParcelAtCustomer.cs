a39f0b5e72e1e7a0503a915ba88a25891deb11af
yaeldoch <92118469+yaeldoch@users.noreply.github.com>
2021-11-03 15:30:01 +0200

    Add classes

---
+++ dotNet5782_9171_7973/BL/ParcelAtCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class ParcelAtCustomer
+    {
+        public int Id { get; set; }
+        public WeightCategory Weight { get; set; }
+        public Priority Priority { get; set; }
+        public ParcelState State { get; set; }
+        public int MyProperty { get; set; }
+        public CustomerInDelivery OtherCustomer { get; set; }
+
+    }
+}
---
+++ dotNet5782_9171_7973/BL/ParcelInDeliver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class ParcelInDeliver
+    {
+        public int ID { get; set; }
+        public WeightCategory Weight { get; set; }
+        public Priority Priority { get; set; }
+        public bool Position { get; set; }
+        public Location Collect { get; set; }
+        public Location Target { get; set; }
+        public double DeliveryDistance { get; set; }
+       
+
+    }
+}

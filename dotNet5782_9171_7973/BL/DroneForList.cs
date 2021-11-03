86ddff0f1262a5c809af7ba1f6d505bf9999811e
dasizis <92228123+dasizis@users.noreply.github.com>
2021-11-03 16:14:00 +0200

    add DroneState enum

---
+++ dotNet5782_9171_7973/BL/DroneForList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    class DroneForList
+    { 
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public WeightCategory MaxWeight { get; set; }
+        public double BatteryState { get; set; }
+        public 
+    }
+}

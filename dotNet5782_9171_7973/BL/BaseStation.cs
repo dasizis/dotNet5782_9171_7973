ca4eee0af3572f1c8b373b4256a9b22bf6361536
yaeldoch <92118469+yaeldoch@users.noreply.github.com>
2021-11-03 16:06:32 +0200

    Added Basic Data Classes

---
+++ dotNet5782_9171_7973/BL/BaseStation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class BaseStation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Location Location { get; set; }
+        public int EmptyChargeSlots { get; set; }
+        public List<Drone> DronesInChargeList { get; set; }
+    }
+}
---
+++ dotNet5782_9171_7973/BL/Drone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class Drone
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public WeightCategory MaxWeight { get; set; }
+        public double BatteryState { get; set; }
+        public DroneState State { get; set; }
+        public ParcelInDeliver ParcelInDeliver { get; set; }
+        public Location Location { get; set; }
+
+
+    }
+}

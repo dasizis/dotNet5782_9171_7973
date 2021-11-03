64d1d603d1773174b2d5c2e63835e574f464e501
yaeldoch <92118469+yaeldoch@users.noreply.github.com>
2021-11-03 16:15:06 +0200

    Added BaseStationToString class

---
+++ dotNet5782_9171_7973/BL/BaseStationForList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    public class BaseStationForList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public int EmptyChargeSlots { get; set; }
+        public int BusyChargeSlots { get; set; }
+    }
+}

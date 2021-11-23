9693d20bd73365798021a23162da7c1ef1c122b8
dasizis <dasizisman480@gmail.com>
2021-11-23 17:43:02 +0200

    fix to stringProps

--- dotNet5782_9171_7973/StringUtilities/StringExtension.cs
+++ dotNet5782_9171_7973/StringUtilities/StringExtension.cs
@@ -1,39 +1,35 @@
 ﻿using System;
+using System.Collections;
 
 namespace StringUtilities
 {
     public static class StringUtilitiesExtension
     {
-        public static string ToStringProps<T>(this T obj)
+        public static string ToStringProps<T>(this T obj, bool detailedNested = false, int nestLevel = 0)
         {
             Type type = obj.GetType();
-            string description = $"{type.Name}";
+            string description = "=============================" +
+                                $"{type.Name}" +
+                                 "=============================";
 
             foreach (var prop in type.GetProperties())
             {
-                description += $"{Environment.NewLine}{prop.Name} = {prop.GetValue(obj)}";
+                description += $"{Environment.NewLine}{prop.Name} = ";
+
+
+                // Is the prop a list?
+                if (prop.PropertyType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(prop.PropertyType))
+                {
+                    if (detailedNested)
+                    {
+
+                    }
+                    else description += $"List<{prop.GetType().GetGenericArguments()}>{}"
+                }
+                else description += prop.GetValue(obj).ToString();
             }
 
             return description;
         }
     }
-
-    
-        public static string ToStringProps<T>(this T obj)
-        {
-            Type type = obj.GetType();
-            string description = $"{type.Name}";
-
-            foreach (var prop in type.GetProperties())
-            {
-                // Is the prop a list?
-                //if (prop.PropertyType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(prop.PropertyType))
-                //{
-
-                //}
-                description += $"{Environment.NewLine}{prop.Name} = {prop.GetValue(obj)}";
-            }
-
-            return description;
-        }
 }

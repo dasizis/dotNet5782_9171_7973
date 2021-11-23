ba8c80c2a22cdf7615cc193bc3a86ff617be7d97
hitmachut <hitmachut@students.local>
2021-11-23 15:58:15 +0200

    create the stringUtilities project

---
+++ dotNet5782_9171_7973/StringUtilities/StringExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StringUtilities
+{
+    public static class StringUtilitiesExtension
+    {
+        public static string ToStringProps<T>(this T obj)
+        {
+            Type type = obj.GetType();
+            string description = $"{type.Name}";
+
+            foreach (var prop in type.GetProperties())
+            {
+                description += $"{Environment.NewLine}{prop.Name} = {prop.GetValue(obj)}";
+            }
+
+            return description;
+        }
+    }
+
+    
+        public static string ToStringProps<T>(this T obj)
+        {
+            Type type = obj.GetType();
+            string description = $"{type.Name}";
+
+            foreach (var prop in type.GetProperties())
+            {
+                // Is the prop a list?
+                //if (prop.PropertyType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(prop.PropertyType))
+                //{
+
+                //}
+                description += $"{Environment.NewLine}{prop.Name} = {prop.GetValue(obj)}";
+            }
+
+            return description;
+        }
+}
---
+++ dotNet5782_9171_7973/StringUtilities/StringUtilities.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringUtilities
+{
+    public class StringUtilities
+    {
+        /// <summary>
+        /// prints a given enum
+        /// each value in the following format:
+        /// (numeric-value) - value
+        /// </summary>
+        /// <param name="enumType">the type of the enum</param>
+        /// <param name="numOfTabs">tabs before each enum value</param>
+        public static string EnumToString(Type enumType)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var option in Enum.GetValues(enumType))
+            {
+                result.Append($"\n{(int)option} - {CamelCaseToReadable(option.ToString())}");
+            }
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Converts a cameCase string to readable text 
+        /// Example: CamelCaseText -> Camel case text
+        /// </summary>
+        /// <param name="camelCase">the camelCase string</param>
+        /// <returns>the readable text</returns>
+        static string CamelCaseToReadable(string camelCase)
+        {
+            char[] letters = camelCase.ToCharArray();
+            string result = "";
+
+            foreach (var l in letters)
+            {
+                string letter = l.ToString();
+                result += letter == letter.ToUpper() ? $" {letter}" : letter;
+            }
+
+            return letters[0] + result[2..];
+        }
+
+        /// <summary>
+        /// prints a title
+        /// </summary>
+        /// <param name="title">the title string</param>
+        public static string TitleFormat(string title)
+        {
+            return "=================================" +
+                   title +
+                   "=================================";
+        }
+    }
+}

4062ec0f98e3baed51c5a57bf5817c5cd2149c5d
dasizis <92228123+dasizis@users.noreply.github.com>
2021-11-27 21:42:11 +0200

    validate name

--- dotNet5782_9171_7973/BL/IBL/BO/BaseStationForList.cs
+++ dotNet5782_9171_7973/BL/IBL/BO/BaseStationForList.cs
@@ -10,7 +10,19 @@ namespace IBL.BO
     public class BaseStationForList
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        string name;
+        public string Name 
+        { 
+            get => name;
+            set
+            {
+                if (!Validation.IsValidName(value))
+                {
+                    thorw new ArgumentException();
+                }
+                name = value;
+            }
+        }
         public int EmptyChargeSlots { get; set; }
         public int BusyChargeSlots { get; set; }
         public override string ToString() => this.ToStringProps();

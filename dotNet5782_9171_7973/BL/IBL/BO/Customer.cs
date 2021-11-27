1d38f6225a438e16de5e5a9f247a04ade7fa8ff2
dasizis <92228123+dasizis@users.noreply.github.com>
2021-11-27 21:42:33 +0200

    validate name

--- dotNet5782_9171_7973/BL/IBL/BO/Customer.cs
+++ dotNet5782_9171_7973/BL/IBL/BO/Customer.cs
@@ -11,7 +11,19 @@ namespace IBL.BO
     public class Customer: ILocalable
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        string name;
+        public string Name 
+        { 
+            get => name;
+            set
+            {
+                if (!Validation.IsValidName(value))
+                {
+                    thorw new ArgumentException();
+                }
+                name = value;
+            }
+        }
         string phone;
         public string Phone 
         { 

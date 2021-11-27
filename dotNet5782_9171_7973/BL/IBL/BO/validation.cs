68b979702654026e48ebdb0e8304e0656a8a0192
yaeldoch <92118469+yaeldoch@users.noreply.github.com>
2021-11-27 21:43:41 +0200

    Correction

---
+++ dotNet5782_9171_7973/BL/IBL/BO/validation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringUtilities;
+
+namespace IBL.BO
+{
+    static class Validation
+    {
+        static bool IsValidPhone(string phone)
+        {
+            foreach (char ch in phone)
+            {
+                if (ch == '-') continue;
+                if (!Char.IsDigit(ch)) return false;
+            }
+            return true;
+        }
+        static bool IsValidLongitude(int longitude)
+        {
+            return longitude >= -180 && longitude <= 180;
+        }
+        static bool IsValidLatitude(int lat)
+        {
+            return lat >= -90 && lat <= 90;
+        }
+        static bool IsValidName(string name)
+        {
+            if (name.Split(' ').Length != 2) return false;
+
+            foreach (char ch in name)
+            {
+                if (ch == ' ') continue;
+                if (!Char.IsLetter(ch)) return false;
+            }
+            return true;
+        }
+
+    }
+}

22971f7d2a9b89850e96d72e69238b63c1f11f1f
dasizis <dasizisman480@gmail.com>
2021-12-15 19:51:15 +0200

    start converter

---
+++ dotNet5782_9171_7973/PL/DroneStateToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PL
+{
+
+    class DroneStateToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values[0] == values[1] ? Visibility.Visible: Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+

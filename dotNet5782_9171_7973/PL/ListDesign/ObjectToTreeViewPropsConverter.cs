f284940a384169927ab46dab1ced71bf18545f5d
dasizis <dasizisman480@gmail.com>
2021-12-16 16:52:46 +0200

    customizse list

---
+++ dotNet5782_9171_7973/PL/ListDesign/ObjectToTreeViewPropsConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PL
+{
+    class ObjectToTreeViewPropsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            return value.GetType()
+                        .GetProperties()
+                        .Select(prop =>
+                        {
+                            var propValue = prop.GetValue(value);
+                            if (propValue?.GetType() == typeof(BO.Location))
+                            {
+                                return new TreeViewLocationProp() { Location = propValue as BO.Location } as Prop;
+                            }
+
+                            return new TreeViewProp() { PropName = prop.Name, propValue = propValue } as Prop;
+                        });
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class LocationToOrientaionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var location = (value as TreeViewLocationProp).Location;
+            return new List<Orientation>() { new Longitude() { Long = location.Longitude }, new Latitude() { Lat = location.Latitude } };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dotNet5782_9171_7973/PL/ListDesign/TreeViewProp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    class Prop { };
+    class TreeViewProp : Prop
+    {
+        public string PropName { get; set; }
+        public object propValue { get; set; }
+    }
+
+    class TreeViewLocationProp : Prop
+    {
+        public BO.Location Location { get; set; }
+    }
+
+    class Orientation {  };
+    class Longitude: Orientation { public double Long { get; set; } }
+    class Latitude: Orientation { public double Lat { get; set; } }
+}

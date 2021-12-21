49241f4359483743f356fdcf8b277e82ea760ccd
dasizis <dasizisman480@gmail.com>
2021-12-21 09:18:05 +0200

    add IDeletable interface

---
+++ dotNet5782_9171_7973/DlApi/DO/IDeletable.cs
@@ -0,0 +1,10 @@
+﻿namespace DO
+{
+    /// <summary>
+    /// An interface which describe the ability to be deleted
+    /// </summary>
+    interface IDeletable
+    {
+        bool IsDeleted { get; set; }
+    }
+}

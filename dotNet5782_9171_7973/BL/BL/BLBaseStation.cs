1b5f932841410413c57007f6c7026c2be89932ad
dasizis <dasizisman480@gmail.com>
2021-12-21 13:56:51 +0200

    update BLBaseStation

---
+++ dotNet5782_9171_7973/BL/BL/BLBaseStation.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+
+namespace BL
+{
+    partial class BL
+    {
+        public void AddBaseStation(int id, string name, Location location, int chargeSlots)
+        {
+            var station = new BaseStation()
+            {
+                Id = id,
+                Name = name,
+                Location = location,
+                EmptyChargeSlots = chargeSlots,
+                DronesInChargeList = new(),
+            };
+
+            try
+            {
+
+                dal.Add(
+                    new DO.BaseStation()
+                    {
+                        Id = station.Id,
+                        Name = station.Name,
+                        Latitude = station.Location.Latitude,
+                        Longitude = station.Location.Longitude,
+                        ChargeSlots = station.EmptyChargeSlots + station.DronesInChargeList.Count,
+                    }
+                );
+            }
+            catch (DO.IdAlreadyExistsException)
+            {
+                throw new IdAlreadyExistsException(typeof(BaseStation), id);
+            }
+        }
+
+        public BaseStation GetBaseStation(int id)
+        {
+            DO.BaseStation baseStation;
+            try
+            {
+                baseStation = dal.GetById<DO.BaseStation>(id);
+            }
+            catch
+            {
+                throw new ObjectNotFoundException(typeof(BaseStation), id);
+            }
+
+            var chargeSlots = dal.GetList<DO.DroneCharge>().Where(charge => charge.StationId == id).ToList();
+            var dronesInChargeList = chargeSlots.Select(charge => GetDrone(charge.DroneId)).ToList();
+
+            return new BaseStation()
+            {
+                Id = baseStation.Id,
+                Name = baseStation.Name,
+                Location = new Location() { Latitude = baseStation.Latitude, Longitude = baseStation.Longitude },
+                EmptyChargeSlots = baseStation.ChargeSlots - chargeSlots.Count(),
+                DronesInChargeList = dronesInChargeList,
+            };
+        }
+        
+        public IEnumerable<BaseStationForList> GetBaseStationsList()
+        {
+            return from baseStation in dal.GetList<DO.BaseStation>()
+                   select GetBaseStationForList(baseStation.Id);
+        }
+
+        public IEnumerable<BaseStationForList> GetAvailableBaseStations()
+        {
+            return from station in dal.GetList<DO.BaseStation>()
+                   let dronesCount = dal.GetFilteredList<DO.DroneCharge>(charge => charge.StationId == station.Id)
+                                        .Count()
+                   where station.ChargeSlots > dronesCount
+                   select GetBaseStationForList(station.Id);
+
+        }
+
+        public void UpdateBaseStation(int baseStationId, string name = null, int? chargeSlots = null)
+        {
+            const string nameProperty = "Name";
+            const string chargeSlotsProperty = "ChargeSlots";
+
+            if (name != null)
+            {
+                if (!Validation.IsValidName(name))
+                    throw new InvalidPropertyValueException(nameProperty, name);
+                
+                dal.Update<DO.BaseStation>(baseStationId, nameProperty, name);
+            }
+
+            if (chargeSlots != null)
+            {
+                if (chargeSlots < 0)
+                    throw new InvalidPropertyValueException(chargeSlotsProperty, chargeSlots);
+
+                dal.Update<DO.BaseStation>(baseStationId, chargeSlotsProperty, chargeSlots);
+            }
+        }
+
+        public void DeleteBaseStation(int baseStationId)
+        {
+            try
+            {
+                dal.Delete<DO.BaseStation>(baseStationId);
+            }
+            catch (DO.ObjectNotFoundException e)
+            {
+                throw new ObjectNotFoundException(typeof(BaseStation), e);
+            }
+        }
+    }
+}
--- dotNet5782_9171_7973/BL/BLApi/IBLBaseStation.cs
+++ dotNet5782_9171_7973/BL/BLApi/IBLBaseStation.cs
@@ -10,10 +10,47 @@ namespace BLApi
 {
     public interface IBLBaseStation
     {
+        /// <summary>
+        /// Adds a base station 
+        /// </summary>
+        /// <param name="id">The base station id</param>
+        /// <param name="name">The base station name</param>
+        /// <param name="location">The base station location</param>
+        /// <param name="chargeSlots">The base station number of charge slots</param>
         void AddBaseStation(int id, string name, Location location, int chargeSlots);
-        void UpdateBaseStation(int baseStationId, string name = null, int? chargeSlots = null);
-        IEnumerable<BaseStationForList> GetBaseStationsList();
-        IEnumerable<BaseStationForList> GetAvailableBaseStations();
+
+        /// <summary>
+        /// return specific base station
+        /// </summary>
+        /// <param name="id">id of requested base station</param>
+        /// <returns>base station with id</returns>
         BaseStation GetBaseStation(int id);
+
+        /// <summary>
+        /// return base stations list
+        /// </summary>
+        /// <returns>base stations list</returns>
+        IEnumerable<BaseStationForList> GetBaseStationsList();
+
+        /// <summary>
+        /// return list of base stations with empty charge slots
+        /// </summary>
+        /// <returns>list of base stations with empty charge slots</returns>
+        IEnumerable<BaseStationForList> GetAvailableBaseStations();
+
+        /// <summary>
+        /// update base station details
+        /// </summary>
+        /// <param name="baseStationId">base station to update</param>
+        /// <param name="name">new name</param>
+        /// <param name="chargeSlots">new number of charge slots</param>
+        void UpdateBaseStation(int baseStationId, string name = null, int? chargeSlots = null);
+
+        /// <summary>
+        /// Deletes a base station
+        /// </summary>
+        /// <param name="baseStationId">The base station Id</param>
+        /// <exception cref="ObjectNotFoundException"></exception>
+        void DeleteBaseStation(int baseStationId);
     }
 }

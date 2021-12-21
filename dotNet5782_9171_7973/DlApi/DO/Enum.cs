a86c69bac96b93c8a9e6b304812904da3419bfd0
dasizis <dasizisman480@gmail.com>
2021-12-21 09:33:09 +0200

    document Dal Enum

--- dotNet5782_9171_7973/DlApi/DO/Enum.cs
+++ dotNet5782_9171_7973/DlApi/DO/Enum.cs
@@ -1,11 +1,18 @@
 ﻿namespace DO
 {
+    /// <summary>
+    /// A maximum weight category which a drone can carry
+    /// </summary>
     public enum WeightCategory
     {
         Light,
         Medium,
         Heavy,
     }
+
+    /// <summary>
+    /// A priority of parcel
+    /// </summary>
     public enum Priority
     {
         Regular,

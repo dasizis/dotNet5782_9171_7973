2814c415d96d875744fde688e70524cb9f92d2da
dasizis <dasizisman480@gmail.com>
2021-12-21 09:18:19 +0200

    document Dal base station

--- dotNet5782_9171_7973/DlApi/DalApi/IDal.cs
+++ dotNet5782_9171_7973/DlApi/DalApi/IDal.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 
 namespace DalApi
 {
+    /// <summary>
+    /// The dal layer interface
+    /// </summary>
     public interface IDal
     {
         (double, double, double, double, double) GetElectricityConfumctiol();
+
         void Add<T>(T item) where T : DO.IIdentifiable;
+
         IEnumerable<T> GetList<T>();
+
         T GetById<T>(int id) where T : DO.IIdentifiable;
+
         void ChargeDrone(int droneId, int stationId);
+
         void FinishCharging(int droneId);
+
         void Update<T>(int id, string attribute, object newValue) where T : DO.IIdentifiable;
+
         int GetParcelContNumber();
+
         IEnumerable<T> GetFilteredList<T>(Func<T, bool> predicate);
     }
 }

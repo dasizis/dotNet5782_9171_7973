e540dabd7f3dc635dfb7dba3ad0328167255bebf
yaeldoch <92118469+yaeldoch@users.noreply.github.com>
2021-12-23 23:04:27 +0200

    Combine Failure and Success into Alert

---
+++ dotNet5782_9171_7973/PL/Alert.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for Alert.xaml
+    /// </summary>
+    public partial class Alert : UserControl
+    {
+        public string Text { get; set; }
+        public bool IsSuccess { get; set; }
+
+        bool isFailure = false;
+        public bool IsFailure { get => !IsSuccess }
+
+        public Alert()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}

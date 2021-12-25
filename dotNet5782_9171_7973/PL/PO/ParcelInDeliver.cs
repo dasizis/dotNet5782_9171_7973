c6d256641862b34a16faf60d04992c7987d251c8
yaeldoch <yaeldoch@gmail.com>
2021-12-25 23:01:24 +0200

    Little changes

--- dotNet5782_9171_7973/PL/PO/ParcelInDeliver.cs
+++ dotNet5782_9171_7973/PL/PO/ParcelInDeliver.cs
@@ -5,7 +5,7 @@ namespace PO
     /// <summary>
     /// A class to represent a PDS of parcel in deliver
     /// </summary>
-    public class ParcelInDeliver
+    class ParcelInDeliver
     {
         /// <summary>
         /// Parcel Id

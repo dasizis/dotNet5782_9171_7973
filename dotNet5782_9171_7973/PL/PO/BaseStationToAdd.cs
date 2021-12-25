a9ff0ad7083d4fe0ca8e2916ba7cbf4cdd08f7c9
yaeldoch <yaeldoch@gmail.com>
2021-12-25 19:58:43 +0200

    Add PO BaseStationToAdd

---
+++ dotNet5782_9171_7973/PL/PO/BaseStationToAdd.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PO
+{
+    class BaseStationToAdd : PropertyChangedNotification
+    {
+        int? id;
+        /// <summary>
+        /// Base Station Id
+        /// </summary>
+        [Required(ErrorMessage = "Required")]
+        public int? Id
+        {
+            get => id;
+            set => SetField(ref id, value);
+        }
+
+        string name;
+        /// <summary>
+        /// Base station name
+        /// </summary>       
+        [Required(ErrorMessage = "Required")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Name length must be between 3-10")]
+        public string Name
+        {
+            get => name;
+            set => SetField(ref name, value);
+        }
+
+        BO.Location location;
+        /// <summary>
+        /// Base station location
+        /// </summary>       
+        [Required(ErrorMessage = "Required")]
+        public BO.Location Location
+        {
+            get => location;
+            set => SetField(ref location, value);
+        }
+
+        int? chargeSlots;
+        /// <summary>
+        /// Base Station Id
+        /// </summary>
+        [Required(ErrorMessage = "Required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Charge slots number should be greater than zero")]
+        public int? ChargeSlots
+        {
+            get => chargeSlots;
+            set => SetField(ref chargeSlots, value);
+        }
+    }
+}

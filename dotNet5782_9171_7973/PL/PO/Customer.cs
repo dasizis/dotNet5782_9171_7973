461fa993d007d1d100ef3d52ca05479d1c86e9b2
yaeldoch <yaeldoch@gmail.com>
2021-12-25 21:00:42 +0200

    Add PO Customer

---
+++ dotNet5782_9171_7973/PL/PO/Customer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using StringUtilities;
+
+
+namespace PO
+{
+    /// <summary>
+    /// A class to represent a PDS of customer
+    /// </summary>
+    class Customer : PropertyChangedNotification
+    {
+        /// <summary>
+        /// Customer Id
+        /// </summary>
+        public int Id { get; set; }
+
+        string name;
+        /// <summary>
+        /// Customer name
+        /// </summary>
+        public string Name
+        {
+            get => name;
+            set => SetField(ref name, value);
+        }
+
+        string phone;
+        /// <summary>
+        /// Customer phone number
+        /// </summary>
+        public string Phone
+        {
+            get => phone;
+            set => SetField(ref phone, value);
+        }
+
+        /// <summary>
+        /// Customer location
+        /// </summary>
+        public Location Location { get; set; }
+
+        /// <summary>
+        /// List of parcels sent from customer
+        /// </summary>
+        public List<Parcel> Send { get; set; }
+
+        /// <summary>
+        /// List of parcels sent to customer
+        /// </summary>
+        public List<Parcel> Recieve { get; set; }
+
+        /// <summary>
+        /// Uses an outer project <see cref="StringUtilities"/>
+        /// to override the <code>ToString()</code> method
+        /// </summary>
+        /// <returns>String representation of customer</returns>
+        public override string ToString() => this.ToStringProperties();
+
+    }
+}

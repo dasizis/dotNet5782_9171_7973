564e21469ec84c82ac7ce9f26c6362123d9fcb27
yaeldoch <yaeldoch@gmail.com>
2021-12-25 21:00:16 +0200

    Add PO BaseStation

---
+++ dotNet5782_9171_7973/PL/PO/BaseStation.cs
@@ -0,0 +1,43 @@
+﻿using StringUtilities;
+using System.Collections.Generic;
+
+namespace PO
+{
+    /// <summary>
+    /// A class to represent a PDS of base station
+    /// </summary>
+    class BaseStation : PropertyChangedNotification
+    {
+        /// <summary>
+        /// Base station Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Base station name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Base station location
+        /// </summary>
+        public Location Location { get; set; }
+
+        /// <summary>
+        /// Number of empty charge slots in base station
+        /// </summary>
+        public int EmptyChargeSlots { get; set; }
+
+        /// <summary>
+        /// List of drones beeing in charged at base station
+        /// </summary>
+        public List<Drone> DronesInCharge { get; set; }
+
+        /// <summary>
+        /// Uses an outer project <see cref="StringUtilities"/>
+        /// to override the <code>ToString()</code> method
+        /// </summary>
+        /// <returns>String representation of customer</returns>
+        public override string ToString() => this.ToStringProperties();
+    }
+}

9d6a271c3b11013da3548a5abec3e079ac5f8e77
dasizis <dasizisman480@gmail.com>
2021-12-26 01:47:56 +0200

    add input behaviors

---
+++ dotNet5782_9171_7973/PL/Views/Behaviors/DoubleInplutBehavior.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows.Controls;
+
+namespace PL.Views.Behaviors
+{
+    class DoubleInputBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.PreviewTextInput += (s, e) =>
+            {
+                if (!double.TryParse(e.Text, out double number))
+                    e.Handled = true;
+            };
+        }
+    }
+}

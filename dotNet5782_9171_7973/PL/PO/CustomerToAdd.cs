36beca7359b3e90a2d17713bec248e6c749a316a
yaeldoch <yaeldoch@gmail.com>
2021-12-25 19:57:59 +0200

    Add PO CustomerToAdd

---
+++ dotNet5782_9171_7973/PL/PO/CustomerToAdd.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO
+{
+    
+    //public void AddCustomer(int id, string name, string phone, Location location)
+
+    class CustomerToAdd : PropertyChangedNotification
+    {
+        int? id;
+        /// <summary>
+        /// Customer Id
+        /// </summary>
+        [Required(ErrorMessage = "Required")]
+        public int? Id
+        {
+            get => id;
+            set => SetField(ref id, value);
+        }
+
+        string name;
+        /// <summary>
+        /// Customer name
+        /// </summary>       
+        [Required(ErrorMessage = "Required")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Name length must be between 3-10")]
+        public string Name
+        {
+            get => name;
+            set => SetField(ref name, value);
+        }
+
+        string phone;
+        /// <summary>
+        /// Customer name
+        /// </summary>       
+        [Required(ErrorMessage = "Required")]
+        [Phone(ErrorMessage = "Phone length must be 10")]
+        public string Phone
+        {
+            get => phone;
+            set => SetField(ref phone, value);
+        }
+
+
+        BO.Location location;
+        /// <summary>
+        /// Customer location
+        /// </summary>       
+        [Required(ErrorMessage = "Required")]
+        public BO.Location Location
+        {
+            get => location;
+            set => SetField(ref location, value);
+        }
+
+    }
+}

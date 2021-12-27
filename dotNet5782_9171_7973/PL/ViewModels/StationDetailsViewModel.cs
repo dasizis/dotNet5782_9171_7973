4b762dc79e327dbde24bdc69bfaa9675110d1eca
yaeldoch <yaeldoch@gmail.com>
2021-12-27 16:36:58 +0200

    Station Details V & VM before correcting

---
+++ dotNet5782_9171_7973/PL/ViewModels/StationDetailsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using PO;
+
+namespace PL.ViewModels
+{
+    class StationDetailsViewModel 
+    {
+        public BaseStation station { get; set; }
+        public RelayCommand<object> UpdateDetails { get; set; }
+        public RelayCommand<object> OpenDronesList { get; set; }
+
+        public StationDetailsViewModel(int id)
+        {
+            station = PLService.GetBaseStation(id);
+
+            UpdateDetails = new RelayCommand<object>(ExecuteUpdateDetails, param => station.Error == null);
+            OpenDronesList = new RelayCommand<object>(ExecuteOpenDronesList, param => station.Error == null);
+
+            BaseStationsNotification.BaseStationsChangedEvent += LoadStation;
+        }
+
+        private void ExecuteUpdateDetails(object param)
+        {
+            PLService.UpdateBaseStation(station.Id, station.Name, station.EmptyChargeSlots);
+        }
+
+        private void ExecuteOpenDronesList(object param)
+        {
+            MessageBox.Show("Open drones list");
+        }
+
+        private void LoadStation(object param)
+        {
+            station = PLService.GetBaseStation(station.Id);
+        }
+    }
+}

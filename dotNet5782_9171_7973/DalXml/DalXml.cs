85c7734b7be59580c4246a985ee150f15883237a
dasizis <dasizisman480@gmail.com>
2021-12-27 17:22:16 +0200

    change vm imports

--- dotNet5782_9171_7973/DalXml/DalXml.cs
+++ dotNet5782_9171_7973/DalXml/DalXml.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using DO;
-using System.Linq;
-using Singleton;
-using System.Reflection;
-using System.IO;
-using System.Xml.Linq;
-using System.Xml.Serialization;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using DO;
+//using System.Linq;
+//using Singleton;
+//using System.Reflection;
+//using System.IO;
+//using System.Xml.Linq;
+//using System.Xml.Serialization;
+//using System.Text;
 
-namespace Dal
-{ 
+//namespace Dal
+//{ 
 
-    /// <summary>
-    /// Implements the <see cref="DalApi.IDal"/> interface using objects to store the data
-    /// </summary>
-    public sealed partial class DalXml : Singleton<DalXml>, DalApi.IDal
-    {
-        private DalXml() { }
+//    /// <summary>
+//    /// Implements the <see cref="DalApi.IDal"/> interface using objects to store the data
+//    /// </summary>
+//    public sealed partial class DalXml : Singleton<DalXml>, DalApi.IDal
+//    {
+//        private DalXml() { }
 
-        static  DalXml() { }
+//        static  DalXml() { }
 
-        private string XmlFilesLocation => $@"{Directory.GetCurrentDirectory()}\..\..\..\..\XmlData";
+//        private string XmlFilesLocation => $@"{Directory.GetCurrentDirectory()}\..\..\..\..\XmlData";
 
-        private string GetXmlFilePath(Type type) => $@"{XmlFilesLocation}\${type.Name}.xml";
+//        private string GetXmlFilePath(Type type) => $@"{XmlFilesLocation}\${type.Name}.xml";
 
-        private string ConfigFilePath => $@"{XmlFilesLocation}\config.xml";
+//        private string ConfigFilePath => $@"{XmlFilesLocation}\config.xml";
 
-        #region Create
+//        #region Create
 
-    }
-}
+//    }
+//}

13563c8d17d12330b555d19f8475452b10e80330
yaeldoch <yaeldoch@gmail.com>
2021-12-30 14:36:21 +0200

    Add ListViewModel

---
+++ dotNet5782_9171_7973/PL/ViewModels/ListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL.ViewModels
+{
+    class ListViewModel<T>
+    {
+        public ObservableCollection<T> list { get; set; }
+
+        public ListViewModel(Predicate<T> predicate)
+        {
+           foreach(var item in GetList().Where(item => predicate(item)))
+            {
+                list.Add(item);
+            }
+            
+        }
+
+        protected abstract IEnumerable<T> GetList();
+    }
+}

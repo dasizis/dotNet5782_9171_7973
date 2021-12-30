d24f29ba4d53be5123b052ea9ff57793d5033bb9
yaeldoch <yaeldoch@gmail.com>
2021-12-30 16:27:17 +0200

    Add temp header param

--- dotNet5782_9171_7973/PL/ViewModels/CustomerDetailsViewModel.cs
+++ dotNet5782_9171_7973/PL/ViewModels/CustomerDetailsViewModel.cs
@@ -23,9 +23,9 @@ namespace PL.ViewModels
             DeleteCommand = new(Delete, CanBeDeleted);
             UpdateCommand = new(Update, () => Customer.Error == null);
             //TODO
-            OpenSentParcelsListCommand = new(() => Views.WorkspaceView.AddPanelCommand.Execute(Workspace.ParcelsListPanel((p) => p.SenderName == Customer.Name)),
+            OpenSentParcelsListCommand = new(() => Views.WorkspaceView.AddPanelCommand.Execute(Workspace.ParcelsListPanel((p) => p.SenderName == Customer.Name, "A")),
                                     () => Customer.Send.Count != 0);
-            OpenSentParcelsListCommand = new(() => Views.WorkspaceView.AddPanelCommand.Execute(Workspace.ParcelsListPanel((p) => p.TargetName == Customer.Name)),
+            OpenRecievedParcelsListCommand = new(() => Views.WorkspaceView.AddPanelCommand.Execute(Workspace.ParcelsListPanel((p) => p.TargetName == Customer.Name, "B")),
                                     () => Customer.Recieve.Count != 0);
         }
 
--- dotNet5782_9171_7973/PL/ViewModels/Workspace.cs
+++ dotNet5782_9171_7973/PL/ViewModels/Workspace.cs
@@ -49,8 +49,8 @@ namespace PL.ViewModels
         public static Panel DronesListPanel(Predicate<PO.DroneForList> predicate) =>
             new(PanelType.List, new Views.DronesListView(predicate), ListPanelName(typeof(PO.Drone)));
 
-        public static Panel ParcelsListPanel(Predicate<PO.ParcelForList> predicate) =>
-            new(PanelType.List, new Views.ParcelsListView(predicate), ListPanelName(typeof(PO.Parcel)));
+        public static Panel ParcelsListPanel(Predicate<PO.ParcelForList> predicate, string header) =>
+            new(PanelType.List, new Views.ParcelsListView(predicate), header);
 
     }
 }

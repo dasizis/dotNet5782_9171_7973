6c8cc992a5fa61b64d3256a98f3889ce34d262d4
yaeldoch <yaeldoch@gmail.com>
2021-12-30 16:02:03 +0200

    Add ParcelsListView & VM

---
+++ dotNet5782_9171_7973/PL/ViewModels/ParcelsListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using PO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL.ViewModels
+{
+    class ParcelsListViewModel : ListViewModel<ParcelForList>
+    {
+        public ParcelsListViewModel(Predicate<ParcelForList> predicate) : base(predicate) { }
+
+        protected override void ExecuteOpen(ParcelForList item)
+        {
+            Views.WorkspaceView.AddPanelCommand.Execute(Workspace.ParcelPanel(item.Id));
+        }
+
+        protected override IEnumerable<ParcelForList> GetList()
+        {
+            return PLService.GetParcelsList();
+        }
+    }
+}

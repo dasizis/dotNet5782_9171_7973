c9d417eedd6ae357227cded944548b723f7fd11f
yaeldoch <yaeldoch@gmail.com>
2021-12-30 19:41:30 +0200

    Add Parcel view

---
+++ dotNet5782_9171_7973/PL/ViewModels/ParcelViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PL.ViewModels
+{
+    class ParcelViewModel: ContentControl
+    {
+        public ParcelViewModel(int id)
+        {
+            Content = new ParcelDetailsViewModel(id);
+           
+        }
+
+        public ParcelViewModel()
+        {
+            Content = new AddParcelViewModel();
+        }
+    }
+}

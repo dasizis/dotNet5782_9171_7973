1d7a10e987b5a81e8e18e2caba88c566afa38c7e
ChayaAnko <chaya.anko@gmail.com>
2021-12-31 10:06:52 +0200

    Implement Close in ParcelsListViewModel

--- dotNet5782_9171_7973/PL/ViewModels/ParcelsListViewModel.cs
+++ dotNet5782_9171_7973/PL/ViewModels/ParcelsListViewModel.cs
@@ -11,14 +11,18 @@ namespace PL.ViewModels
     {
         public ParcelsListViewModel(Predicate<ParcelForList> predicate) : base(predicate) { ParcelsNotification.ParcelsChangedEvent += LoadList; }
 
+
         protected override void ExecuteOpen(ParcelForList item)
         {
             Views.WorkspaceView.AddPanelCommand.Execute(Workspace.ParcelPanel(item.Id));
         }
-
         protected override IEnumerable<ParcelForList> GetList()
         {
             return PLService.GetParcelsList();
+        }
+        protected override void Close()
+        {
+            Views.WorkspaceView.RemovePanelCommand.Execute(Workspace.ListPanelName(typeof(Parcel)));
         }
     }
 }

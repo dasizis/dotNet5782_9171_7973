b110cda7c0c55ea0121ed710e9625249ac663418
yaeldoch <yaeldoch@gmail.com>
2022-01-01 21:15:02 +0200

    Add Customer VM

---
+++ dotNet5782_9171_7973/PL/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace PL.ViewModels
+{
+    class CustomerViewModel : ContentControl
+    {
+        public CustomerViewModel()
+        {
+            Content = new AddCustomerViewModel();
+        }
+        public CustomerViewModel(int id)
+        {
+            Content = new CustomerDetailsViewModel(id);
+        }
+    }
+}

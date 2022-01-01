04e16d888802b9e4476d9985a372c014fd56b917
yaeldoch <yaeldoch@gmail.com>
2022-01-01 21:15:26 +0200

    Add Drone VM

---
+++ dotNet5782_9171_7973/PL/ViewModels/DroneViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace PL.ViewModels
+{
+    class DroneViewModel : ContentControl
+    {
+        public DroneViewModel()
+        {
+            Content = new AddDroneViewModel();
+        }
+        public DroneViewModel(int id)
+        {
+            Content = new DroneDetailsViewModel(id);
+        }
+    }
+}

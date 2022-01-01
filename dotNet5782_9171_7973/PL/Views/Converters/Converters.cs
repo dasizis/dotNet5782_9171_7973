4d0c9327a670370f6383f74719a8bde82e6bce7d
yaeldoch <yaeldoch@gmail.com>
2022-01-01 22:13:13 +0200

    Add converter fot tooltip content

--- dotNet5782_9171_7973/PL/Views/Converters/Converters.cs
+++ dotNet5782_9171_7973/PL/Views/Converters/Converters.cs
@@ -118,5 +118,18 @@ namespace PL.Views.Converters
         }
     }
 
+    public class DateToToolTipConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value ?? "Not yet";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 
 }

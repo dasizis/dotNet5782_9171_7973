550a9e8d36b245469e5968756c3eaf08abb1a9f3
yaeldoch <yaeldoch@gmail.com>
2022-01-01 21:15:17 +0200

    Add Station VM

---
+++ dotNet5782_9171_7973/PL/ViewModels/BaseStationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace PL.ViewModels
+{
+    class BaseStationViewModel : ContentControl
+    {
+        public BaseStationViewModel()
+        {
+            Content = new AddBaseStationViewModel();
+        }
+        public BaseStationViewModel(int id)
+        {
+            Content = new StationDetailsViewModel(id);
+        }
+    }
+}

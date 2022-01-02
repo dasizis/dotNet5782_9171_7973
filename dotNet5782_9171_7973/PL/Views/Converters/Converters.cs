dc60a817b7bae56aaec29121c3a05a94ab6e0ede
dasizis <dasizisman480@gmail.com>
2022-01-02 22:25:01 +0200

    customize

--- dotNet5782_9171_7973/PL/Views/Converters/Converters.cs
+++ dotNet5782_9171_7973/PL/Views/Converters/Converters.cs
@@ -92,7 +92,7 @@ namespace PL.Views.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (PO.DroneState)value == PO.DroneState.Maintenance ? 30 : 1;
+            return (PO.DroneState)value == PO.DroneState.Maintenance ? 60 : 1;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)

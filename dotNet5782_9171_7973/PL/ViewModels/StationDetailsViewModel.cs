4e409a67755341f0bdf173d46da81b7af2908e67
yaeldoch <yaeldoch@gmail.com>
2022-01-06 16:01:26 +0200

    Change droneslistPanel

--- dotNet5782_9171_7973/PL/ViewModels/StationDetailsViewModel.cs
+++ dotNet5782_9171_7973/PL/ViewModels/StationDetailsViewModel.cs
@@ -24,7 +24,7 @@ namespace PL.ViewModels
 
             UpdateDetailsCommand = new(ExecuteUpdateDetails, () => Station.Error == null);
             DeleteCommand = new(Delete, () => Station.DronesInCharge.Count == 0);
-            OpenDronesListCommand = new(() => Views.WorkspaceView.AddPanelCommand.Execute(Workspace.DronesListPanel((d) => Station.DronesInCharge.Exists(drone => drone.Id == d.Id))),
+            OpenDronesListCommand = new(() => Views.WorkspaceView.AddPanelCommand.Execute(Workspace.DronesListPanel((d) => Station.DronesInCharge.Exists(drone => drone.Id == d.Id), Workspace.StationChrgedDronesName(Station.Id))),
                                     () => Station.DronesInCharge.Count != 0);
         }
 

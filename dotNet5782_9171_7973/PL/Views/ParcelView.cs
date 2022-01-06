48d13ea7743ca1fa12a559a977ada259c21a9441
yaeldoch <yaeldoch@gmail.com>
2022-01-06 22:03:21 +0200

    Remove and place views in proper folder

---
+++ dotNet5782_9171_7973/PL/Views/ParcelView.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace PL.Views
+{
+    class ParcelView: ContentControl
+    {
+        public ParcelView()
+        {
+            Content = new ViewModels.AddParcelViewModel();
+        }
+
+        public ParcelView(int id)
+        {
+            Content = new ViewModels.ParcelDetailsViewModel(id);
+        }
+    }
+}

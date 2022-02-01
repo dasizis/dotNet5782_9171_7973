12fbc85eb18267468a0a1b7ecf6da9db42c605f8
dasizis <dasizisman480@gmail.com>
2022-02-01 19:48:20 +0200

    make temp notification files

---
+++ dotNet5782_9171_7973/PL/PL/CustomerNotification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace PL
+{
+    static class CustomerNotification
+    {
+        /// <summary>
+        /// Occurs when one or more values were modified
+        /// </summary>
+        public static event Action ValueChanged;
+
+        /// <summary>
+        /// Used to notify from outer class the one or more values were modified
+        /// </summary>
+        /// <param name="callerMethodName">The caller method name</param>
+        public static void NotifyValueChanged([CallerMemberName] string callerMethodName = "")
+        {
+            ValueChanged?.Invoke();
+        }
+    }
+}
